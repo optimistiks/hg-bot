50d108b318e5f090e0bfad540d2718cde62bd62a
Ansirius <sandking13@yandex.ru>
2015-12-01 12:30:27 +0300

    Update index.js

--- handlers/index.js
+++ handlers/index.js
@@ -6,5 +6,10 @@ module.exports = [
   require('./affirmative'),
   require('./hello'),
   require('./daaaaa'),
-  require('./default')
+  require('./default'), 
+  require('./localmems/Tolstoy'), 
+  require('./localmems/bot1'),
+  require('./localmems/subscribers'),
+  require('./localmems/5'),
+  require('./localmems/Night'),
 ];
