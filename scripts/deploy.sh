25bd0b1b4ab593523948fa328f9613d0474055ff
Potapov Max <mpmsp@yandex-team.ru>
2015-12-03 16:15:54 +0300

    debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,7 @@ echo $SSH_PASS
 echo $SSH_CONN
 echo $PROJECT_PATH
 export SSHPASS=$SSH_PASS
-sshpass -e ssh $SSH_CONN $PROJECT_PATH/deploy.sh
+echo $SSHPASS
+OUTPUT=`sshpass -e ssh $SSH_CONN $PROJECT_PATH/deploy.sh`
+echo "DONE"
+echo $OUTPUT
