b59c871cc65b1a6fb297a43019e4d8898eb710f5
Potapov Max <mpmsp@yandex-team.ru>
2015-12-03 16:41:46 +0300

    output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 export SSHPASS=$SSH_PASS
-sshpass -e ssh $SSH_CONN $PROJECT_PATH/deploy.sh > /dev/null 2>&1
-echo "DONE"
+OUTPUT="$(sshpass -e ssh $SSH_CONN $PROJECT_PATH/deploy.sh)"
+echo "${OUTPUT}"
